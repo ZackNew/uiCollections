be3d625e3ca55f56c18c37e380db03a05dd8087c
zekariyas <zekariyasmulugeta91@gmail.com>
2023-06-27 20:34:10 +0300

    few changes

--- components/ecommerce/FilterBar.vue
+++ components/ecommerce/FilterBar.vue
@@ -9,11 +9,26 @@
           name="trade-assurance"
           class="rounded mr-2 focus:ring-0"
         />
-        <label for="trade-assurance">Trade Assurance</label>
+        <label for="trade-assurance" class="flex items-center"
+          ><span
+            ><img
+              src="/assets/images/crown-svgrepo-com.svg"
+              class="w-5 h-5 mr-2"
+              alt=""
+          /></span>
+          Trade Assurance</label
+        >
       </div>
       <div class="mt-2 flex items-center text-gray-500">
         <input type="checkbox" name="verified-suppliers" class="rounded mr-2" />
-        <label for="verified-suppliers">Verified Suppliers</label>
+        <label for="verified-suppliers" class="flex items-center"
+          ><span
+            ><img
+              src="/assets/images/verified.png"
+              class="w-5 h-5 mr-2"
+              alt="" /></span
+          >Verified Suppliers</label
+        >
       </div>
     </div>
     <div class="mb-6">
@@ -47,6 +62,17 @@
         <input type="checkbox" name="verified-suppliers" class="rounded mr-2" />
         <label for="verified-suppliers">Second hand</label>
       </div>
+    </div>
+    <div class="w-full">
+      <label for="range" class="font-bold">Min Order</label>
+      <input
+        type="range"
+        name="price"
+        min="1"
+        max="100"
+        value="10"
+        class="w-full h-2 bg-blue-400 appearance-none"
+      />
     </div>
   </div>
 </template>
