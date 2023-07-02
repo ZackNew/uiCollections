9aaf8fc8ffc37300fbb0a6ee03a18f7529f6dddf
zekariyas <zekariyasmulugeta91@gmail.com>
2023-07-02 21:19:44 +0300

    hero started

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="uppercase text-orange-600">
+  <div class="container flex justify-around items-center">
+    <div class="w-[65%]">
+      <h1 class="uppercase text-orange-600 font-bold">
         best destination around the world
       </h1>
+      <h1 class="mt-6 text-7xl lg:text-8xl font-bold text-blue-950">
+        Travel, enjoy <br />
+        and live a new <br />
+        and full life
+      </h1>
+      <p class="mt-12">
+        Built Wicket longer admire do barton vanity itself do in it. <br />
+        prefered to sportsmen it engrossed listening. Park gate <br />
+        sell they west hard for the.
+      </p>
+      <div class="flex items-center mt-8">
+        <button class="bg-yellow-500 py-3 px-5 rounded text-white mr-5">
+          Find out more
+        </button>
+        <div class="flex items-center justify-between">
+          <i class="mdi mdi-play-circle text-5xl text-orange-500"></i>
+          <h1 class="font-bold">Play Demo</h1>
+        </div>
+      </div>
     </div>
-    <div></div>
+    <div>
+      <img class="" src="~/assets/images/landingHero.png" alt="" />
+    </div>
   </div>
 </template>
 
